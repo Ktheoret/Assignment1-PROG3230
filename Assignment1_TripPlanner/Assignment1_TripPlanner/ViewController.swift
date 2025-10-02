3a0de4b226f92c0b2907cb5c76c34264bb6d48fc
kristinT <kristin.theoret@gmail.com>
2025-10-02 12:53:53 -0400

    Recreated Xcode project in repo

---
+++ Assignment1_TripPlanner/Assignment1_TripPlanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment1_TripPlanner
+//
+//  Created by Kristin Theoret on 2025-10-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
